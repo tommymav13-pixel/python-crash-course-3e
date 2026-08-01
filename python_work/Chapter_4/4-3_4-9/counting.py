3b78ceff97230a1b62ee9489e215f8f62affb46a
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-01 16:49:37 +0200

    Chapter 4 progress

---
+++ python_work/Chapter_4/4-3_4-9/counting.py
@@ -0,0 +1,3 @@
+# Create a for loop that counts from 1 to 20, and print the numbers in the loop.
+for value in range(1, 21):
+    print(value)
---
+++ python_work/Chapter_4/4-3_4-9/counttomillion.py
@@ -0,0 +1,3 @@
+# Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing ctrl-C or by closing the output window.)
+for value in range(1, 1000001):
+    print(value)    
