46f58bfbb9d96f730023ecc7b75a2aef2b40b3bb
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-01 16:55:45 +0200

    Chapter 4 progress

---
+++ python_work/Chapter_4/4-3_4-9/threes.py
@@ -0,0 +1,13 @@
+threes = []
+for value in range(3, 31):
+    three = value * 3
+    threes.append(three)
+print(threes)
+
+threes = []
+for value in range(3, 31):
+    threes.append(value * 3)
+print(threes)   
+
+threes = [value * 3 for value in range(3, 31)]
+print(threes)   
