cb813f86027605341e69373576cdbd8355b54757
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-03 00:48:00 +0200

    Chapter 5 progress

---
+++ python_work/Chapter_5/voting.py
@@ -0,0 +1,7 @@
+age = 19
+if age >= 18:
+    print("You are eligible to vote.")
+    print("Have you registered to vote yet?")
+else:
+    print("You are not eligible to vote yet.")
+    print("Please register to vote when you turn 18.")
