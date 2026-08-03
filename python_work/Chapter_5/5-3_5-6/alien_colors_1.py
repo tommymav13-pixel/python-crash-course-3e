93760fcd0c1873c2e053f9af16201a9975e1a503
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-03 20:51:59 +0200

    Chapter 5 progress

---
+++ python_work/Chapter_5/5-3_5-6/alien_colors_1.py
@@ -0,0 +1,20 @@
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+    print("You earned 5 points!")
+else:
+    print("No points earned.") 
+
+alien_color = ['green', 'yellow', 'red']
+if 'yellow' in alien_color:
+    print("You earned 10 points!")      
+
+alien_color = ['green', 'yellow', 'red']
+if 'red' in alien_color:
+    print("You earned 15 points!") 
+
+# Write a version of this program that fails
+alien_color = ['green', 'yellow', 'red']
+if 'blue' in alien_color:
+    print("You earned 20 points!")  
+else:
+    print("No points earned.")
---
+++ python_work/Chapter_5/5-3_5-6/alien_colors_2.py
@@ -0,0 +1,15 @@
+# Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+alien_color = 'green'
+if alien_color == 'green':
+    print("You earned 5 points for shooting the alien!")    
+# If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("You earned 5 points for shooting the alien!")
+else:
+    print("You earned 10 points for shooting the alien!")   
+# If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+alien_color = 'red'
+if alien_color == 'green':  
+    print("You earned 5 points for shooting the alien!")    
+# Write one version of this program that runs the if block and another that runs the else block             
