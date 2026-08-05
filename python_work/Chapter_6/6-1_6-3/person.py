17cf68bb07fc970a897bec2aea0ed3c360f68db8
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-05 21:10:52 +0200

    Update Chapter 6 6-1_6-3

---
+++ python_work/Chapter_6/6-1_6-3/person.py
@@ -0,0 +1,7 @@
+person = {'first_name': 'John', 'last_name': 'Doe', 'age': 30,
+          'city': 'New York'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
