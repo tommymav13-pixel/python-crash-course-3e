a3ee030b15a1a037d6dc887142c425831c9b973a
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-06 20:33:35 +0200

    Add rivers file

---
+++ python_work/Chapter_6/6-4_6-6/rivers.py
@@ -0,0 +1,13 @@
+# rivers in the world
+rivers = []
+# or
+rivers = {'Nile': 'Egypt', 'Amazon': '', 'Yangtze': ''}
+
+for river, country in rivers.items():
+    print(f"The {river} runs through {country}.")
+
+for river in rivers.keys():
+    print(river)
+
+for country in rivers.values():
+    print(country)
