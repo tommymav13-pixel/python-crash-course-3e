c98121149eeb8484eb06c2ed3032f5400a2f269d
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-09 21:28:26 +0200

    Add cities to Chapter 6

---
+++ python_work/Chapter_6/6-7_6-12/cities.py
@@ -0,0 +1,27 @@
+cities = {
+    'amsterdam': {
+        'population': 'high',
+        'country': 'netherlands',
+        'fact': 'The Royal Palace on Dam Square alone is held up by 13,659 wooden poles.'
+    },
+    'london': {
+        'population': 'normal',
+        'country': 'engeland',
+        'fact': 'The London Underground is the oldest underground railway system in the world'
+    },
+    'madrid': {
+        'population': 'normal',
+        'country': 'spain',
+        'fact': 'Madrid is the highest capital city in Europe, sitting 650 meters (2,133 feet) above sea level on a central plateau.'
+    }
+}
+
+for city, city_info in cities.items():
+    print(f"\nCity: {city.title()}")
+    population = f"{city_info['population']}"
+    country = f"{city_info['country']}"
+    fact = city_info["fact"]
+
+    print(f"\tCountry: {country.title()}")
+    print(f"\tPopulation: {population.title()}")
+    print(f"\tFact: {fact.title()}")
