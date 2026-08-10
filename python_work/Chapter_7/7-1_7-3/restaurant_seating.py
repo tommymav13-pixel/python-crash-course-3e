ab8cccac6073978c53d32fd59c0d3aa53d721971
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-10 09:58:00 +0200

    Add seating to Ch 7

---
+++ python_work/Chapter_7/7-1_7-3/restaurant_seating.py
@@ -0,0 +1,7 @@
+seating = input("How many people are in your dinner group: ")
+seating = int(seating)
+
+if seating >= 8:
+    print("\nYou have to wait for a table")
+else:
+    print("\nTable is ready!")
