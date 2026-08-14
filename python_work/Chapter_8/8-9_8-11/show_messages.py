a8da231d7f13828699f50a75b7fd7e80e93f6198
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-14 20:23:25 +0200

    Add show messages function chapter 8

---
+++ python_work/Chapter_8/8-9_8-11/show_messages.py
@@ -0,0 +1,15 @@
+messages = [
+    "I love Python",
+    "Java is fun!",
+    "I wish to become a programmer!",
+    "I am interested in linear algebra!",
+    "Probability and Statistics is my cup of tea",
+    "I cannot wait until I can start with NumPy",
+]
+
+def show_messages(messages):
+    """Show text messages."""
+    for message in messages:
+        print(message)
+
+show_messages(messages)
