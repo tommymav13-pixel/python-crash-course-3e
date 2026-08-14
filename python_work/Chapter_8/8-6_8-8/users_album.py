63bd835d0f2fc107a4e00ac688d7b0a0d223469a
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-14 10:28:18 +0200

    Add users album

---
+++ python_work/Chapter_8/8-6_8-8/users_album.py
@@ -0,0 +1,20 @@
+def make_album(artist_name, album_title, album_number = None):
+        """Return a dictionary of information about an album."""
+        album_01 = {'artist': artist_name, 'title': album_title}
+
+        if album_number: 
+            album_01['number'] = album_number
+        return album_01
+
+album = make_album('metallica', 'master of puppets', 'battery')
+print(album)
+
+album = make_album('ed sheeran', 'play', 'a little more')
+print(album)
+
+album = make_album('linkin park', 'the emptiness machine', 
+                   'the emptiness machine')
+print(album)
+
+# create a while loop that allows users to enter an album’s artist and title.
+# think prompt and input()
