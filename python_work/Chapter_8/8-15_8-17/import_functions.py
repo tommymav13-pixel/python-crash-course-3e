4d96be4406f7cf46ef7d52c4a149d2b5599d0786
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-19 00:16:05 +0200

    Add pets to import to chapter 8

--- python_work/Chapter_8/8-15_8-17/import_functions.py
+++ python_work/Chapter_8/8-15_8-17/import_functions.py
@@ -1,6 +1,25 @@
-import module_name
+import pets_to_import
+pets_to_import.describe_pet(animal_type='hamster', pet_name='harry')
+
+
 from module_name import function_name
 from module_name import function_name as fn
 import module_name as mn
 from module_name import *
 
+
+
+describe_pet('willie', 'dog')
+describe_pet('cat', 'sheba')
+
+# Order Matters in Positional Arguments. The order of Keyword arguments doesn't
+# matters
+
+# Equivalent Function Calls
+# A dog named Willie.
+describe_pet('dog', 'willie')
+describe_pet('dog', pet_name='willie')
+# A hamster named Harry.
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
