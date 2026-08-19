b05db01f9726702c36e12128113afb89f8e066b6
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-19 21:24:20 +0200

    Update class dog chapter 9

--- python_work/Chapter_9/dog.py
+++ python_work/Chapter_9/dog.py
@@ -1,18 +1,18 @@
 class Dog:
-        """A simple attempt to model a dog."""
+    """A simple attempt to model a dog."""
 
-        def __init__(self, name, age):
-                """Initialize name and age attributes."""
-                self.name = name
-                self.age = age
+    def __init__(self, name, age):
+            """Initialize name and age attributes."""
+            self.name = name
+            self.age = age
 
-        def sit(self):
-                """Simulate a dog sitting in response to a command"""
-                print(f"{self.name} is now sitting.")
+    def sit(self):
+            """Simulate a dog sitting in response to a command"""
+            print(f"{self.name} is now sitting.")
 
-        def roll_over(self):
-                """Simulate rolling over in response to a command."""
-                print(f"{self.name} rolled over!")
+    def roll_over(self):
+            """Simulate rolling over in response to a command."""
+            print(f"{self.name} rolled over!")
 
 my_dog = Dog('Willie', 6)
 your_dog = Dog('Lucy', 3)
