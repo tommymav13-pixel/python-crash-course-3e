167eef3228910a5529c9174fa6a08c863c37e2aa
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-19 21:23:11 +0200

    Update class restaurant chapter 9

--- python_work/Chapter_9/9-1/class_restaurant.py
+++ python_work/Chapter_9/9-1/class_restaurant.py
@@ -1,24 +1,27 @@
 class Restaurant:
-        """A simple attempt to model information about a restaurant"""
+    """A simple attempt to model information about a restaurant."""
 
-        def __init__(self, name, cuisine):
-                """Initialize name and type"""
-                self.name = name
-                self.cuisine = cuisine
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initialize restaurant name and cuisine type."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
 
-        def describe_restaurant(self):
-                """Print restaurant description"""
-                print(f"The restaurant's name is {self.name.title()} and has a"
-                      f"{self.cuisine} kitchen")
+    def describe_restaurant(self):
+        """Print the restaurant description."""
+        print(
+            f"The restaurant's name is {self.restaurant_name.title()} "
+            f"and serves {self.cuisine_type.title()} cuisine."
+        )
 
-        def open_restaurant(self):
-                """Simulate opening the restaurant."""
-                print(f"{self.name.title()} is now open!")
+    def open_restaurant(self):
+        """Simulate opening the restaurant."""
+        print(f"{self.restaurant_name.title()} is now open!")
+
 
 my_restaurant = Restaurant("IDA", "japanese")
 
-print(f"My favourite restaurant is {my_restaurant.name}!")
-print(f"They have {my_restaurant.cuisine} foods")
+print(f"My favourite restaurant is {my_restaurant.restaurant_name}!")
+print(f"They serve {my_restaurant.cuisine_type} cuisine.")
 
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
