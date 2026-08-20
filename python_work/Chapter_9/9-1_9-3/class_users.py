350c40f8e53916674a46a110105790501bc444b1
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-20 15:28:14 +0200

    Add user class to chapter 9

---
+++ python_work/Chapter_9/9-1_9-3/class_users.py
@@ -0,0 +1,11 @@
+class User:
+    """Gives description of user"""
+
+    def __init__(self, first_name, describe_user, greet_user):
+        """Initialize user descriptions"""
+        self.first_name = first_name
+        self.describe_user = describe_user
+        self.greet_user = greet_user
+
+    def call_name():
+        
