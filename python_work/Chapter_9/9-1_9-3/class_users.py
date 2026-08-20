6b430c63828ac74257290079db26e8bbb4efcd68
tommymav13-pixel <tommy.mav13@gmail.com>
2026-08-20 17:47:15 +0200

    Update class users in chapter 9

--- python_work/Chapter_9/9-1_9-3/class_users.py
+++ python_work/Chapter_9/9-1_9-3/class_users.py
@@ -1,11 +1,23 @@
 class User:
     """Gives description of user"""
 
-    def __init__(self, first_name, describe_user, greet_user):
+    def __init__(self, first_name, last_name):
         """Initialize user descriptions"""
         self.first_name = first_name
-        self.describe_user = describe_user
-        self.greet_user = greet_user
+        self.last_name = last_name
 
-    def call_name():
-        
+    def describe_user(self):
+        """Print user discription"""
+        print(
+            f"First name: {self.first_name}"
+            f"Last name: {self.last_name}"
+            )
+
+    def greet_user(self):
+        """Greet the user"""
+        print(f"Welcome {self.first_name} {self.last_name}!")
+
+user_01 = User("Richard", "Dawkins")
+
+user_01.describe_user()
+user_01.greet_user()
